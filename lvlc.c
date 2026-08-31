5094d026dccf9a3f9f6a91cfd57110b015f01da9
dotdok132 <illot3617@gmail.com>
2026-08-31 19:44:59 +0300

    feat: Integrate lvlang-string plugin (LibID 0x08) into core lvlc runtime

--- lvlc.c
+++ lvlc.c
@@ -12,6 +12,7 @@
 #include "../lvlang-keyboard/keyboard_plugin.c"
 #include "../lvlang-sdl2/sdl2_plugin.c"
 #include "../lvlang-time/time_plugin.c"
+#include "../lvlang-string/string_plugin.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -261,6 +262,7 @@ int main(int argc, char **argv) {
     lvl_plugin_keyboard_init(&vm);
     lvl_plugin_sdl2_init(&vm);
     lvl_plugin_time_init(&vm);
+    lvl_plugin_string_init(&vm);
 
     if (trace_execution) {
         printf("\n--- SINGLE-STEP VM EXECUTION TRACE ---\n");
